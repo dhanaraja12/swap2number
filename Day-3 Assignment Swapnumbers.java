a33a5dcdb8e10c005a477790ab7a7fae6b04643b
dhanaraja12 <114278558+dhanaraja12@users.noreply.github.com>
2022-09-24 12:30:34 +0530

    day3 assignment swap of two number complete

---
+++ Day-3 Assignment Swapnumbers.java
@@ -0,0 +1,15 @@
+
+public class Swapnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1,b=2;
+		System.out.println("before swaping a=1  and b=2");
+		int c=a;
+		a=b;
+		b=c;
+		System.out.println("After swaping a="+a+" and b=" +b);
+	}
+	
+
+}
